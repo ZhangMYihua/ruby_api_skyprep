d0f6c38769c3afeef52090de53c1760748b3c8d3
Yihua Zhang <michaelzhang2@msn.com>
2015-10-05 12:38:39 -0400

    initializing class of skyprepapi with faraday connection to skyprepapi url

--- api.rb
+++ api.rb
@@ -1,1 +1,23 @@
-conn = Faraday.new(:url => 'https://api.skyprep.io/admin/api')
+require 'faraday'
+
+API_URL = 'https://api.skyprep.io/admin/api'
+
+conn = Faraday.new(:url => API_URL) do |faraday|
+	faraday.request  :url_encoded             
+  faraday.response :logger                  
+  faraday.adapter  Faraday.default_adapter 
+end
+
+response = Faraday.get('#{API_URL}')
+
+class SkyPrepApi
+
+	def initialize(api_key, acct_key)
+		@api_key = api_key
+		@acct_key = acct_key
+	end
+
+
+
+
+end
