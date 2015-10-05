69743d2df8b7018f4b835e784eb052c5f449d79a
Yihua Zhang <michaelzhang2@msn.com>
2015-10-05 15:31:12 -0400

    adding meta_argument to convert user argument to match api argument if able

--- api.rb
+++ api.rb
@@ -6,22 +6,43 @@ class SkyPrepApi
 	def initialize(api_key, acct_key)
 		@api_key = api_key
 		@acct_key = acct_key
-		@conn = Faraday.new(:url => 'https://api.skyprep.io/admin/api') 
+		@base_url = Faraday.new(:url => 'https://api.skyprep.io/admin/api') 
 		@hash = {:api_key => @api_key, :acct_key => @acct_key}	
 	end
 
 	def test_connection
-		response = @conn.get 'test_connection', @hash
+		response = @base_url.get 'test_connection', @hash
 		JSON.parse(response.body)
 	end
 
-	def get_users
-		response = @conn.get 'get_users', @hash
+	def get(url, hash = {})
+		params = @hash.merge(hash)
+		response = @base_url.get url, params
 		JSON.parse(response.body)
 	end
 
+	def post(url, hash = {})
+		params = @hash.merge(hash)
+		response = @base_url.get url, params
+		JSON.parse(response.body)
+	end
+
+	def method_missing(method_call, args = {})
+		string = method_call.id2name
+		if post_prefixes.any? {|prefix| string.include?(prefix)}
+			self.post(string, args)
+		elsif string.include?('get')
+			self.get(string, args)
+		else
+			raise "Issue connecting to API. Please check all parameters"
+		end
+	end
+
+private
+	get_prefixes = ['get']
+	post_prefixes = ['update', 'destroy', 'create']
 end
 
 my_app = SkyPrepApi.new('GjNHirhJC0TZ1nmJFtHP0IWfT', 'learnabli.skyprepapp.com')
 
-puts my_app.get_users
+puts my_app.post('create_user', {'email' => 'dubbs2@msn.com', 'first_name' => 'Jason'})
