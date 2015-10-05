db5da30e0a0802a744a5f4d77d1129001edc7580
Yihua Zhang <michaelzhang2@msn.com>
2015-10-05 13:22:47 -0400

    adding get_users method and test_connection

--- api.rb
+++ api.rb
@@ -6,20 +6,21 @@ class SkyPrepApi
 	def initialize(api_key, acct_key)
 		@api_key = api_key
 		@acct_key = acct_key
-		@conn = Faraday.new(:url => 'https://api.skyprep.io/admin/api') do |faraday|
-			faraday.request  :url_encoded             
-		  faraday.response :logger                  
-		  faraday.adapter  Faraday.default_adapter 
-		end
+		@conn = Faraday.new(:url => 'https://api.skyprep.io/admin/api')
 	end
 
-	def authenticate
+	def test_connection
+		response = @conn.get 'https://api.skyprep.io/admin/api/test_connection', {:api_key => @api_key, :acct_key => @acct_key}
+		JSON.parse(response.body)
 	end
 
 	def get_users
-		users = @conn.get '/get_users', {"api_key" => @api_key, "acct_key" => @acct_key}
-		JSON.parse(users.body)
+		response = @conn.get 'https://api.skyprep.io/admin/api/get_users', {:api_key => @api_key, :acct_key => @acct_key}
+		JSON.parse(response.body)
 	end
 
-
 end
+
+my_app = SkyPrepApi.new('GjNHirhJC0TZ1nmJFtHP0IWfT', 'learnabli.skyprepapp.com')
+
+puts my_app.get_users
