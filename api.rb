50be038c16f7070c0b36c6e267cc62aabaaa16a0
Yihua Zhang <michaelzhang2@msn.com>
2015-10-15 12:18:24 -0400

    fixed post method from get to post, though both work for skyprep api

--- api.rb
+++ api.rb
@@ -29,7 +29,7 @@ class SkyPrepApi
 		else
 			params = @hash
 		end
-		response = @base_url.get url, params
+		response = @base_url.post url, params
 		JSON.parse(response.body)
 	end
 
@@ -48,4 +48,6 @@ class SkyPrepApi
 
 end
 
+my_app = SkyPrepApi.new('GjNHirhJC0TZ1nmJFtHP0IWfT' ,'learnabli.skyprepapp.com')
 
+puts my_app.test_connection
